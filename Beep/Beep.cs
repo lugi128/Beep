6da55a4bf007d3fba03b6689fd2e8aafa516632f
Sadek Amrouche <Sadek@Default-Computer.local>
2015-03-08 17:30:38 +0100

    Beep project

---
+++ Beep/Beep.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Beep
+{
+    class Beep
+    {
+        static public void Alarm(int hz, int time, int elapse, string exception)
+        {
+            if (hz >= 37)
+            {
+                try
+                {
+                    Console.Beep(hz, time * 1000);
+                    if (elapse == 0)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Thread.Sleep(elapse * 1000);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+    }
+}
---
+++ Beep/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool secok = false;
+            bool hzok = false;
+            int hz = 0;
+            int time = 0;
+            do
+            {
+                try
+                {
+                    Console.Write("Entrer une fréquence en Hertz (entier) : "); 
+                    hz = Convert.ToInt32(Console.ReadLine());
+                    hzok = true;
+                }
+                catch
+                {
+                    hzok = false;
+                }
+            } while (!hzok);
+            
+            do
+            {
+                try
+                {
+                    Console.Write("Pendant combien de temps en seconde (entier) : ");
+                    time = Convert.ToInt32(Console.ReadLine());
+                    secok = true;
+                }
+                catch
+                {
+                    secok = false;
+                }
+            } while (!secok);
+            try
+            {
+                Beep.Alarm(hz, time, 5, "Can't work under 36 Hz. Please reload !");
+            }
+            catch
+            {
+                Console.WriteLine("Doesn't Work !");
+            }
+            Console.ReadKey();
+        }
+    }
+}
